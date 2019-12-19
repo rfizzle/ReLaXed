dd6c4b70d5cf050cf2327ec499259a2eb9c701ae
Coleton Pierson <coleton.pierson@agoda.com>
2019-12-19 10:50:53 +0700

    Updated apexcharts template pug to handle errors.

--- src/builtin_plugins/apexcharts/index.js
+++ src/builtin_plugins/apexcharts/index.js
@@ -2,6 +2,26 @@ const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
 
+let apexchartjsHandler = async function (chartjsPath, page) {
+  let chartSpec = fs.readFileSync(chartjsPath, 'utf8');
+  let html = pug.renderFile(path.join(__dirname, 'template.pug'), {chartSpec});
+  let tempHTML = chartjsPath + '.htm';
+
+  fs.writeFileSync(tempHTML, html);
+  await page.setContent(html);
+  await page.waitForFunction(() => (window.svgData || window.svgDataError));
+
+  const svgData = await page.evaluate(() => window.svgData);
+  const svgDataError = await page.evaluate(() => window.svgDataError);
+
+  if (svgData) {
+    const svgPath = chartjsPath.substr(0, chartjsPath.length - '.apexchart.js'.length) + '.svg';
+    fs.writeFileSync(svgPath, svgData);
+  } else {
+    console.log("Error: " + svgDataError)
+  }
+};
+
 exports.constructor = async function (params) {
   return {
     watchers: [
@@ -14,17 +34,3 @@ exports.constructor = async function (params) {
 };
 
 
-var apexchartjsHandler = async function (chartjsPath, page) {
-  var chartSpec = fs.readFileSync(chartjsPath, 'utf8');
-  var html = pug.renderFile(path.join(__dirname, 'template.pug'), { chartSpec });
-  var tempHTML = chartjsPath + '.htm';
-
-  fs.writeFileSync(tempHTML, html);
-  await page.setContent(html);
-  await page.waitForFunction(() => window.svgData);
-
-  const svgData = await page.evaluate(() => window.svgData);
-
-  var svgPath = chartjsPath.substr(0, chartjsPath.length - '.apexchart.js'.length) + '.svg';
-  fs.writeFileSync(svgPath, svgData);
-};
